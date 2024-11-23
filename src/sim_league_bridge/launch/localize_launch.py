36435ae117cff030742b6a69392988b2f74cf396
megalphian <megnath.ramesh@gmail.com>
2024-11-23 18:53:26 -0500

    WIP: EKF localization with lidar

--- src/sim_league_bridge/launch/localize_launch.py
+++ src/sim_league_bridge/launch/localize_launch.py
@@ -12,12 +12,37 @@ def generate_launch_description():
         'online_node.launch.py'
     )
 
+    ekf_launch_file = os.path.join(
+        get_package_share_directory('robot_localization'),
+        'launch',
+        'ekf.launch.py'
+    )
+
+    ekf_params_file = os.path.join(
+        get_package_share_directory('sim_league_bridge'),
+        'config',
+        'ekf_params.yaml'
+    )
+
     ld = LaunchDescription([
+        Node(
+            package='sim_league_bridge',
+            executable='vehicle_odom',
+            name='vehicle_odom',
+            output='screen',
+        ),
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[ekf_params_file],
+           ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(kinematic_icp_file),
             launch_arguments={
                 'lidar_topic': "/autodrive/f1tenth_1/lidar",
-                'tf_timeout': 0.4
+                'tf_timeout': '0.4'
             }.items()
         )
     ])
