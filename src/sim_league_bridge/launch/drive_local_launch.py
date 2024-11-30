459268407ff4eb963db41153affec1e8796f2f4d
megalphian <megnath.ramesh@gmail.com>
2024-11-30 13:59:10 -0500

    Add simulator bringup to launch file for submission

--- src/sim_league_bridge/launch/drive_local_launch.py
+++ src/sim_league_bridge/launch/drive_local_launch.py
@@ -1,10 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 import os
 
 def generate_launch_description():
 
+    # Path to the other launch file
+    other_launch_file = os.path.join(
+        get_package_share_directory('autodrive_f1tenth'),
+        'launch',
+        'simulator_bringup_headless.launch.py'
+    )
+
     ld = LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(other_launch_file),
+        ),
         Node(
             package='sim_league_bridge',
             executable='vehicle_controller.py',
